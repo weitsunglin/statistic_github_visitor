c80fa8c1eb901ea290b5999a3fe6335c9ec6b348
OLD_RD2林瑋琮 <wilson.lwt.tw@gmail.com>
2024-05-11 12:49:15 +0800

    Update get_repo_unique_views.py

--- get_repo_unique_views.py
+++ get_repo_unique_views.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+
 api_key = os.getenv("API_KEY")
 
 def get_all_repos_unique_views(username, token, exclude_repo=None):
@@ -10,48 +11,46 @@ def get_all_repos_unique_views(username, token, exclude_repo=None):
     repos_response = requests.get(repos_url, headers=headers)
     repositories = repos_response.json()
     
-    # Check for error messages
     if isinstance(repositories, dict) and repositories.get('message', ''):
         print(repositories['message'])
         return
 
-    # Store unique view data for each repository
     all_unique_views_data = []
 
     for repo in repositories:
         if repo['name'] == exclude_repo:
-            continue  # Skip the specified repository
+            continue
 
         views_url = f"https://api.github.com/repos/{username}/{repo['name']}/traffic/views"
         views_response = requests.get(views_url, headers=headers)
         views_data = views_response.json()
 
-        # Check for error messages
         if "message" in views_data:
             print(f"Error fetching views for {repo['name']}: {views_data['message']}")
             continue
 
-        # Sum up unique view counts
         total_unique_views = sum(view['uniques'] for view in views_data.get('views', []))
         all_unique_views_data.append({'Repository': repo['name'], 'Total Unique Views': total_unique_views})
 
-    # Convert to DataFrame and sort to get the top 10 repositories with the most unique views
     df = pd.DataFrame(all_unique_views_data)
     df = df.sort_values(by="Total Unique Views", ascending=False).head(10)
 
-    # Plot the bar chart
     plt.figure(figsize=(6, 8))
     bars = plt.bar(df["Repository"], df["Total Unique Views"], color='purple')
     plt.xlabel('Repository')
     plt.ylabel('Total Unique Views')
     plt.title('Last two week Top 10 Repositories by Total Unique Views')
-    plt.xticks(rotation=90)  # Rotate x-axis labels for readability
-    plt.tight_layout()  # Adjust layout to avoid squeezing
+    plt.xticks(rotation=90)
+    plt.tight_layout()
 
-    # Add text annotations on each bar for the specific view counts
+    # 設定y軸只有三個刻度
+    max_views = df["Total Unique Views"].max()
+    plt.ylim(0, max_views * 1.1)
+    plt.yticks([0, max_views // 2, max_views])
+
     for bar in bars:
         yval = bar.get_height()
-        plt.text(bar.get_x() + bar.get_width()/2, yval, int(yval), va='bottom', ha='center')  # Vertical and horizontal alignment
+        plt.text(bar.get_x() + bar.get_width()/2, yval, int(yval), va='bottom', ha='center')
 
     plt.savefig('github_visitor.png')
 
